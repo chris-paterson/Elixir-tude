efa0d856e65f6ffb7fd647a4f77b1c78aa86399b
Chris Paterson <CJPaterson.3@gmail.com>
2017-07-17 21:07:36 +0100

    Étude 4-4: Tail Recursion with an Accumulator

--- powers.ex
+++ powers.ex
@@ -2,7 +2,9 @@ defmodule Powers do
   import Kernel, except: [raise: 2]
 
   def raise(_, 0), do: 1
-  def raise(x, 1), do: x
-  def raise(x, n) when n > 0, do: x * raise(x, n - 1)
+  def raise(x, n) when n > 0, do: raise(x, n, 1)
   def raise(x, n) when n < 0, do: 1.0 / raise(x, -n)
+
+  defp raise(_x, 0, acc), do: acc
+  defp raise(x, n, acc), do: raise(x, n - 1, x * acc)
 end

c01da89e1564b0feecca75f514b69cfbd4cc7f90
Chris Paterson <CJPaterson.3@gmail.com>
2017-07-17 19:49:01 +0100

    Étude 3-2: Guards

--- geom.ex
+++ geom.ex
@@ -13,7 +13,13 @@ defmodule Geom do
 
   @spec area(atom(), number(), number()) :: number()
 
-  def area(:rectangle, length, width), do: length * width
-  def area(:triangle, base, height), do: base * height / 2
-  def area(:ellipse, major_radius, minor_radius), do: :math.pi * major_radius * minor_radius
+  def area(:rectangle, length, width) when length >= 0 and width >= 0 do 
+    length * width
+  end
+  def area(:triangle, base, height) when base >= 0 and height >= 0 do 
+    base * height / 2
+  end
+  def area(:ellipse, major_radius, minor_radius) when major_radius >= 0 and minor_radius >= 0 do 
+    :math.pi * major_radius * minor_radius
+  end
 end

dc24a9b0e73d76833adf8961ee29fadbbf89846d
Chris Paterson <CJPaterson.3@gmail.com>
2017-07-17 21:25:42 +0100

    Étude 4-5: Recursion with a Helper Function

--- powers.ex
+++ powers.ex
@@ -7,4 +7,17 @@ defmodule Powers do
 
   defp raise(_x, 0, acc), do: acc
   defp raise(x, n, acc), do: raise(x, n - 1, x * acc)
+
+  def nth_root(x, n), do: nth_root(x, n, x / 2.0)
+  defp nth_root(x, n, approx) do
+    IO.puts("Current guess is #{approx}")
+    f = raise(approx, n) - x
+    f_prime = n * raise(approx, n - 1)
+    next = approx - f / f_prime
+    change = abs(approx - next)
+    cond do
+      change < 1.0e-8 -> next
+      true -> nth_root(x, n, next)
+    end
+  end
 end

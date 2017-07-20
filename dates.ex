4efc9ab3cb00292aba15b339c9b63ba32610b446
Chris Paterson <CJPaterson.3@gmail.com>
2017-07-20 23:08:04 +0100

    Étude 6-2: Accumulating the Sum of a List

--- dates.ex
+++ dates.ex
@@ -1,7 +1,28 @@
 defmodule Dates do
+  
   def date_parts(iso_date) do
     String.split(iso_date, ~r/-/)
     |> Enum.map(&(String.to_integer &1))
   end
-end
 
+  def julian(iso_date) do
+    [year, month, day] = date_parts(iso_date)
+    days_for_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days_for_full_months = month_total(month, days_for_month, 0) + day
+    cond do
+      is_leap_year(year) and month > 2 -> total_days_for_full_months + 1
+      true -> total_days_for_full_months
+    end
+  end
+
+  defp month_total(1, _days_per_month, accumulator), do: accumulator
+  defp month_total(month_number, days_per_month, accumulator) do
+    [days_for_month | remaining_months] = days_per_month
+    month_total(month_number - 1, remaining_months, accumulator + days_for_month)
+  end
+
+  defp is_leap_year(year) do
+    (rem(year,4) == 0 and rem(year,100) != 0)
+    or (rem(year, 400) == 0)
+  end
+end
--- tests/date_test.exs
+++ tests/date_test.exs
@@ -5,4 +5,17 @@ defmodule DatesTest do
   test "date gets correctly split" do
     assert Dates.date_parts("2013-06-15") == [2013, 6, 15]
   end
+
+  test "julian gives correct date" do
+    assert Dates.julian("2013-12-31") == 365
+    assert Dates.julian("2013-02-05") == 36
+    assert Dates.julian("1900-03-01") == 60
+    assert Dates.julian("2013-01-01") == 1
+  end
+
+  test "julian gives correct date for leap year" do
+    assert Dates.julian("2012-12-31") == 366
+    assert Dates.julian("2000-03-01") == 61
+    assert Dates.julian("2012-02-05") == 36
+  end
 end
